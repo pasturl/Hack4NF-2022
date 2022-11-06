c8e89a08cc6e6e9d49ea4e0586cf390ed428663f
pasturl <lucas.pastur@gmail.com>
2022-11-06 01:16:55 +0100

    Create expander in the app

--- app.py
+++ app.py
@@ -43,52 +43,50 @@ with st.expander("ℹ️ - About this app", expanded=False):
     st.markdown("")
 
 st.markdown("")
-st.markdown("## Machine Learning model feature importance using SHAP")
-
 
 st.sidebar.image(img_main, width=240)
 feature_option = st.sidebar.selectbox("Feature", features, index=0)
-
-st.markdown(f"### Importance of {feature_option}")
-st.dataframe(df[df["feature"] == feature_option])
-
 target_option = st.sidebar.selectbox("Model target", targets, index=0)
 
 target = target_option.split("_")[1]
 
-st.markdown(f"### Feature importance for {target}")
-st.dataframe(df[df["model_target"] == target_option])
+with st.expander("Model feature importance using SHAP", expanded=False):
 
-st.markdown("#### SHAP feature importance")
-shap_bar_plot = f"{models_path}{target_option}/shap_class_1_bar_plot.png"
-st.image(shap_bar_plot, width=700)
+    st.markdown(f"### Importance of {feature_option}")
+    st.dataframe(df[df["feature"] == feature_option])
 
-st.markdown("### SHAP feature correlation")
-shap_plot_25 = f"{models_path}{target_option}/shap_class_1_25.png"
-st.image(shap_plot_25, width=700)
+    st.markdown(f"### Feature importance for {target}")
+    st.dataframe(df[df["model_target"] == target_option])
 
-st.markdown("### Model metrics")
-model_lgb_test_auc = f"{models_path}{target_option}/model_lgb_test_auc.png"
-st.image(model_lgb_test_auc, width=700)
+    st.markdown("#### SHAP feature importance")
+    shap_bar_plot = f"{models_path}{target_option}/shap_class_1_bar_plot.png"
+    st.image(shap_bar_plot, width=700)
 
-
-st.header("BERTopic analysis of important genes")
-
-st.markdown("#### Topic heatmap")
-HtmlFile_heatmap = open(f"nlp/{target}/bert_heatmap.html", 'r', encoding='utf-8')
-source_code_heatmap = HtmlFile_heatmap.read()
-print(source_code_heatmap)
-components.html(source_code_heatmap, height=800, scrolling=True)
-
-st.markdown("#### Topic words score")
-HtmlFile_hierarchy = open(f"nlp/{target}/bert_hierarchy.html", 'r', encoding='utf-8')
-source_code_hierarchy = HtmlFile_hierarchy.read()
-print(source_code_hierarchy)
-components.html(source_code_hierarchy, height=800, scrolling=True)
+    st.markdown("### SHAP feature correlation")
+    shap_plot_25 = f"{models_path}{target_option}/shap_class_1_25.png"
+    st.image(shap_plot_25, width=700)
 
 
-st.markdown("#### Topic words score")
-HtmlFile_words = open(f"nlp/{target}/bert_topic_words_score.html", 'r', encoding='utf-8')
-source_code_words = HtmlFile_words.read()
-print(source_code_words)
-components.html(source_code_words, height=1200, scrolling=True)
+with st.expander("Model metrics", expanded=False):
+    model_lgb_test_auc = f"{models_path}{target_option}/model_lgb_test_auc.png"
+    st.image(model_lgb_test_auc, width=700)
+
+with st.expander("BERTopic analysis of important genes", expanded=False):
+    st.markdown("#### Topic heatmap")
+    HtmlFile_heatmap = open(f"nlp/{target}/bert_heatmap.html", 'r', encoding='utf-8')
+    source_code_heatmap = HtmlFile_heatmap.read()
+    print(source_code_heatmap)
+    components.html(source_code_heatmap, height=800, scrolling=True)
+
+    st.markdown("#### Topic words score")
+    HtmlFile_hierarchy = open(f"nlp/{target}/bert_hierarchy.html", 'r', encoding='utf-8')
+    source_code_hierarchy = HtmlFile_hierarchy.read()
+    print(source_code_hierarchy)
+    components.html(source_code_hierarchy, height=800, scrolling=True)
+
+
+    st.markdown("#### Topic words score")
+    HtmlFile_words = open(f"nlp/{target}/bert_topic_words_score.html", 'r', encoding='utf-8')
+    source_code_words = HtmlFile_words.read()
+    print(source_code_words)
+    components.html(source_code_words, height=1200, scrolling=True)
